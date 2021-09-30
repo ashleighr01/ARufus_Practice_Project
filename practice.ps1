499d595ec9be67a5b78fee28b0698c1c926d7ab7
Ashleigh Rufus <ashleighr01@gmail.com>
2021-09-29 23:17:42 -0500

    Some practice first

---
+++ practice.ps1
@@ -0,0 +1,12 @@
+public class Test {
+	public static void main (String [] args) {
+
+		String words;
+		int nums;
+		words = "I am trying my best";
+		nums = 1232;
+		
+		System.out.println(nums + word);
+	}
+
+}
